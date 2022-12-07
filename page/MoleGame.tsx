224942d4d2cc0e7855b3c1d94a67c63b6a70ace0
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-12-07 19:56:08 +0900

    board

--- page/MoleGame.tsx
+++ page/MoleGame.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { styles } from '../StyleSheet'
-import { View } from 'react-native'
+import React from 'react';
+import { styles } from '../StyleSheet';
+import { View } from 'react-native';
 
 const MoleGame = () => {
-    return(<>
-    <View style={styles.MoleBoard}></View>
-    </>)
-} 
+  return (
+    <>
+      <View style={styles.MoleBoard}>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+      </View>
+    </>
+  );
+};
 
-export default MoleGame
+export default MoleGame;
