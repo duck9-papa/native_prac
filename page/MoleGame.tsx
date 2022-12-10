b3a5f3f284fbb3e957c6f0f9dd3bac24b463188f
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-12-10 23:42:06 +0900

    view center

--- page/MoleGame.tsx
+++ page/MoleGame.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { styles } from '../StyleSheet';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 const MoleGame = () => {
   return (
     <>
       <View style={styles.MoleBoard}>
-        <View><Text></Text></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
+        <View style={styles.flexCenter}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>3</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>4</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>5</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>6</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>7</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>8</Text>
+        </View>
+        <View style={styles.flexCenter}>
+          <Text>9</Text>
+        </View>
       </View>
     </>
   );
